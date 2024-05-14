8d2cb64c7a40c96b175a824b9c0572b61dd87b8e
Anthony Patton <anthonyalanpatton@gmail.com>
2024-05-13 21:54:50 -0700

    Implemented Reducer

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,5 +1,30 @@
 import React from "react";
+import Questions from "./Questions";
 
 export default function Quiz() {
-  return <div>Quiz</div>;
+  /* next button event handler */
+  function onNext() {
+    console.log("On next click");
+  }
+
+  /* previous button event handler */
+  function onPrev() {
+    console.log("On onPrev click");
+  }
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz Application</h1>
+
+      {/* display questions */}
+      <Questions />
+      <div className="grid">
+        <button className="btn prev" onClick={onPrev}>
+          Prev
+        </button>
+        <button className="btn next" onClick={onNext}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
